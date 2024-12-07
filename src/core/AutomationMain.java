0a143119e0bb89d0fff82b0b88d1c345627440e3
Yannik Schmiede <yannikschmiede@gmail.com>
2024-12-07 10:58:47 +0100

    Changed version

--- src/core/AutomationMain.java
+++ src/core/AutomationMain.java
@@ -1,7 +1,7 @@
 package core;
 
 public class AutomationMain {
-	private static final String CURRENT_VERSION = "1.3.0";
+	private static final String CURRENT_VERSION = "1.4.0";
 
 	public static void main(String[] args) {
 		Gui.generateGui(CURRENT_VERSION);
