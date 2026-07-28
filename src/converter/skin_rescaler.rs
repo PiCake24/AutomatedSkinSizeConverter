3ade54e4d30548d6b37ffc71b0bf37f9747b3b38
Yannik Schmiede <yannikschmiede@gmail.com>
2026-07-28 12:51:58 +0200

    fixed normal skinscaler

--- src/converter/skin_rescaler.rs
+++ src/converter/skin_rescaler.rs
@@ -2,25 +2,36 @@ use std::collections::VecDeque;
 use std::fs;
 use std::fs::File;
 use std::io::{BufReader, Read};
+use std::sync::mpsc::Sender;
 use serde_json::{json, Value};
+use crate::converter::main_gui::{log, WorkerMessage};
 
-pub fn rescale_skins(champion:&str, champion_parent:&str, skin: u16){
-    //todo write into file
-    // todo special cases lux, djsona
+/// reads a
+pub fn rescale_skins(sender:&Sender<WorkerMessage>, champion:&str, champion_parent:&str, skin: u16, scale: f32) -> Result<(), Box<dyn std::error::Error>>{
+    if champion == "lux" && skin == 7{
+        todo!()
+    } else if champion == "sona" && true{ //todo
+        todo!() //ist djsona nicht einfach djsona?
+    } else {
+        //todo write into file
+        rescale_normal(sender, champion, champion_parent, skin, scale)
+    }
+}
+
+fn rescale_normal(sender:&Sender<WorkerMessage>, champion:&str, champion_parent:&str, skin: u16, scale: f32) -> Result<(), Box<dyn std::error::Error>>{
     let filepath = format!(r"D:\wad5\{}\data\characters\{}\skins\skin{}.json", champion_parent, champion, skin);
-    let scale = get_scale(champion, skin);
 
     let mut data = String::new();
-    let f = File::open(&filepath).expect("File not available");
+    let f = File::open(&filepath).inspect_err(|e| { log(sender, format!("Could not open file: {}", e)) })?;
     let mut br = BufReader::new(&f);
-    br.read_to_string(&mut data).expect("Should be able to read to string");
-    let mut parsed: Value = serde_json::from_str(&data).unwrap(); //todo
+    br.read_to_string(&mut data).inspect_err(|e| { log(sender, format!("Could not read data: {}", e)) })?;
+    let mut parsed: Value = serde_json::from_str(&data).inspect_err(|e| { log(sender, format!("Could convert data to json: {}", e)) })?;
 
     //change and write data
-    let entries = parsed.get_mut("entries").unwrap();
-    traverse(entries, vec![&format!("characters/{}/skins/skin{}",champion, skin), "skinmeshproperties","skinscale"].into(), scale);
-    // print!("{}", serde_json::to_string_pretty(&parsed).unwrap());
-    fs::write(&filepath, serde_json::to_string_pretty(&parsed).unwrap()).expect("Could not write into file");
+    let entries = parsed.get_mut("entries").ok_or_else(|| std::io::Error::new(std::io::ErrorKind::InvalidData, "Missing entries item"))?;
+    traverse(sender, entries, vec![&format!("characters/{}/skins/skin{}",champion, skin), "skinmeshproperties","skinscale"].into(), scale)?;
+    fs::write(&filepath, serde_json::to_string_pretty(&parsed).inspect_err(|e| { log(sender, format!("Could create string: {}", e)) })?).inspect_err(|e| { log(sender, format!("Could not write to file: {}", e)) })?;
+    Ok(())
 }
 
 /// Traverses the json and changes a number according scale, if the last path part does not exist it creates it
@@ -28,75 +39,47 @@ pub fn rescale_skins(champion:&str, champion_parent:&str, skin: u16){
 /// # Arguments
 /// * value: the json that should be traversed
 /// * path: the path that should get traversed
-/// + scale: the size increase of that champion
-fn traverse(value: & mut Value, mut path:VecDeque<&str>, scale:f64){
-    let json_array = value["value"]["items"].as_array_mut().unwrap();
+/// + scale: the size increase of that Champion
+fn traverse(sender:&Sender<WorkerMessage>, value: & mut Value, mut path:VecDeque<&str>, scale:f32) -> Result<(), Box<dyn std::error::Error>>{
+    let json_array = value["value"]["items"].as_array_mut().ok_or_else(|| std::io::Error::new(std::io::ErrorKind::InvalidData, "Missing items array"))?;
     let key = path.pop_front();
-    // println!("Key: {:?}", key);
+    if key.is_none(){
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData, "No key"
+        ).into());
+    }
     for element in json_array.iter_mut(){
-        // println!("{}", element["key"]);
         if element["key"].as_str().is_some() {
-            if element["key"].as_str().unwrap().to_lowercase() == key.unwrap() {
-                // println!("Element: {}", element);
+            if element["key"].as_str().unwrap().to_lowercase() == key.unwrap().to_lowercase() {
                 if path.is_empty() {
-                    element["value"] = Value::Number((scale as u64).into());
-                    return;
+                    element["value"] = json!(scale);
+                    return Ok(());
                 }
-                return traverse(element, path, scale);
+                return traverse(sender, element, path, scale);
             }
         } else if element["key"].as_i64().is_some(){
             if element["key"].as_i64().unwrap().to_string() == key.unwrap() {
-                // println!("Element: {}", element);
                 if path.is_empty() {
-                    element["value"] = Value::Number((scale as u64).into());
-                    return;
+                    element["value"] = json!(scale);
+                    return Ok(());
                 }
-                return traverse(element, path, scale);
+                return traverse(sender, element, path, scale);
             }
         }
-        // println!("Element[key]: {:?}", element["key"])
     }
     if path.is_empty(){
-        println!("scale doesnt exist in json, adding");
+        log(sender, "scale doesnt exist in json, adding");
         let new_entry = json!({
             "key": "skinScale",
             "type": "f32",
             "value": scale
         });
-        json_array.push(new_entry)
+        json_array.push(new_entry);
+        Ok(())
     } else{
-        panic!("json not complete")
+        Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData, "JSON not complete"
+        ).into())
     }
 }
 
-/// todo
-// todo maybe I have to rework some stuff, so it doesnt crash when file does not exist :/
-pub fn get_scale(champion : &str, number : u16) -> f64 {
-    let mut data = String::new();
-    let path_to_file = format!("{}{}{}", r"D:\wad\0PutSizeOptionFilesHere\", champion, ".txt");
-    let f = File::open(path_to_file).expect("There is no size options file");
-    let mut br = BufReader::new(f);
-    br.read_to_string(&mut data).expect("Should be able to read to string");
-    let mut default:f64 = 2.0;
-
-    let rows: Vec<&str> = data.split("\r\n").collect();
-
-    for row in rows {
-        let key_value: Vec<&str> = row.splitn(2, ":").collect();
-        let key: Result<u16, _> = key_value.get(0).unwrap().trim().parse();
-        let mut value: Result<f64, _> = key_value.get(1).unwrap().trim().parse();
-
-        if value.is_ok() {
-            if key.is_ok() {
-                if key.unwrap() == number {
-                    return value.unwrap()
-                }
-            } else {
-                default = value.unwrap();
-            }
-        }
-    }
-
-
-    default
-}
